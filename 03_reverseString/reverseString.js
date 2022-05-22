61db20daec399bf694584b102754de66f2ade6fc
bsoalio <bsoalio@gmail.com>
2022-05-22 09:57:29 +0000

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  let textReverse = "";
+  //loop through the string
+  for (let i = text.length - 1; i > -1; i--) {
+    // add to the reverse string from the last character to the first
+    textReverse += text[i];
+  }
 
+  return textReverse;
 };
 
 // Do not edit below this line
