958bf4bd4795707ecc708676ab8e404a444898b1
bsoalio <bsoalio@gmail.com>
2022-05-22 11:12:12 +0000

    Finish 05_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...theArgs) {
+  //for each elements of theArgs
+  for (const arg of theArgs) {
+    // compare it to the elements of input array
+    for (let i = 0; i < inputArray.length; i++) {
+      // if arr[i] === arg update the value of the new array
+      if (arg === inputArray[i]) {
+        //remove element ot index i from the array
+        inputArray.splice(i, 1);
+      }
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
