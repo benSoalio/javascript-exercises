85e107a93787612799ce0e2cdfd8baa63bd1c072
bsoalio <bsoalio@gmail.com>
2022-06-02 18:25:10 +0000

    Fimish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (elements) {
+  return elements.reduce(
+    (total, currentElement) => add(total, currentElement),
+    0
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (elements) {
+  return elements.reduce((total, currentElement) => total * currentElement);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
